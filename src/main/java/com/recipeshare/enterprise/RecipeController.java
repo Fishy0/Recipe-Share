4e99a06ea05a18e6649e097af101895dd6c5be47
Eric Coomer <ericcoomer1@gmail.com>
2025-09-30 16:05:04 -0400

    Recipe and User controllers

--- src/main/java/com/recipeshare/enterprise/RecipeController.java
+++ src/main/java/com/recipeshare/enterprise/RecipeController.java
@@ -1,7 +1,13 @@
 package com.recipeshare.enterprise;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RecipeController {
+
+    @RequestMapping("RecipeIndex")
+    public String index() {
+            return "RecipeIndex";
+    }
 }
--- src/main/java/com/recipeshare/enterprise/UserController.java
+++ src/main/java/com/recipeshare/enterprise/UserController.java
@@ -1,7 +1,13 @@
 package com.recipeshare.enterprise;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("UserIndex")
+    public String index() {
+        return "UserIndex";
+    }
 }
