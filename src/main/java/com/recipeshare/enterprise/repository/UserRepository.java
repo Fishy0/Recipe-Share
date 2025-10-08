99ee59d3343386b26d25ff3671b7efb9a27b7042
Eric Coomer <ericcoomer1@gmail.com>
2025-10-07 21:15:56 -0400

    Recipe / User Repository

--- src/main/java/com/recipeshare/enterprise/repository/UserRepository.java
+++ src/main/java/com/recipeshare/enterprise/repository/UserRepository.java
@@ -1,2 +1,6 @@
-package com.recipeshare.enterprise.repository;public class UserRepository {
-}
+package com.recipeshare.enterprise.repository;
+
+import com.recipeshare.enterprise.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {}
